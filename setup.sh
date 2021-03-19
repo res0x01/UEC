017020fdd694442fe7d340d01f17ea7ff6c10d80
res0x01 <80387370+res0x01@users.noreply.github.com>
2021-03-19 22:53:35 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat ./base-file-head.md > README.md
+
+cat ./OFFICIAL/twitter.md >> README.md
+cat ./OFFICIAL/links.md >> README.md
+cat ./OFFICIAL/announcements.md >> README.md
+cat ./OFFICIAL/others.md >> README.md
+
+cat ./CLUBS/head.md >> README.md
+cat ./CLUBS/announcements.md >> README.md
+cat ./CLUBS/others.md >> README.md
